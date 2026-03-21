a0fee45f92157c4f697b85b911b6c2daa9579f88
ramonmorillo <ramon.morillo.verdugo@gmail.com>
2026-03-21 11:51:44 +0100

    Redesign scientific foundation footer

--- assets/modules/legal-module.js
+++
@@ -1,9 +0,0 @@
-export function getLegalHighlights(translate) {
-  return [
-    translate('legal.clinicalDisclaimer'),
-    translate('legal.aiDisclaimer'),
-    translate('legal.privacyDisclaimer'),
-    translate('legal.regulatoryDisclaimer'),
-    translate('legal.ipDisclaimer')
-  ];
-}
